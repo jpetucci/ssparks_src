c9cb10689d2156acb19d560ec1eddfbe0cd40833
Carl LeBlond <carl.leblond@iup.edu>
2019-09-30 11:09:13 -0400

    first commit

---
+++ Obj_serial/spparks.d
@@ -0,0 +1,54 @@
+spparks.o: spparks.cpp /usr/include/stdc-predef.h spktype.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/syslimits.h \
+ /usr/include/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/uio_lim.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/inttypes.h ../STUBS/mpi.h /usr/include/string.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h \
+ /usr/include/x86_64-linux-gnu/bits/strings_fortified.h \
+ /usr/include/x86_64-linux-gnu/bits/string_fortified.h spparks.h \
+ /usr/include/stdio.h /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/libio.h \
+ /usr/include/x86_64-linux-gnu/bits/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h memory.h pointers.h error.h \
+ universe.h input.h app.h solve.h domain.h potential.h random_mars.h \
+ timer.h output.h style_app.h app_chemistry.h app_diffusion.h \
+ app_erbium.h app_ising.h app_ising_single.h app_membrane.h app_potts.h \
+ app_potts_additive.h app_potts_grad.h app_potts_neigh.h \
+ app_potts_neighonly.h app_potts_phasefield.h app_potts_pin.h \
+ app_potts_strain.h app_potts_strain_pin.h app_potts_weld.h \
+ app_potts_weld_jom.h app_relax.h app_sinter.h app_sos.h app_test_group.h \
+ style_solve.h solve_group.h solve_linear.h solve_tree.h style_diag.h \
+ diag_array.h diag_cluster.h diag_diffusion.h diag_energy.h diag_erbium.h \
+ diag_propensity.h diag_sinter_density.h diag_sinter_free_energy.h \
+ diag_sinter_free_energy_pore.h style_dump.h dump_image.h dump_sites.h \
+ dump_text.h dump_vtk.h style_pair.h pair_lj_cut.h style_region.h \
+ region_block.h region_cylinder.h region_intersect.h region_sphere.h \
+ region_union.h style_command.h create_box.h create_sites.h read_sites.h \
+ set.h shell.h
